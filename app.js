a6fc38649be87ca65ed58ed41fd0e93953d9959f
sanjay <ksanjayk@protonmail.com>
2022-05-29 11:18:49 +0530

    added tasks route

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
+app.use(express.json({ extended: false }));
+app.use('/api/v1/tasks', tasks);
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
