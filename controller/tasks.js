976c525fc8cd2d46f7a2a9d7c2ba84c360ae5096
sanjay <ksanjayk@protonmail.com>
2022-05-29 21:44:17 +0530

    formated controller

--- controller/tasks.js
+++ controller/tasks.js
@@ -23,54 +23,52 @@ exports.createTask = async (req, res) => {
 exports.getTask = async (req, res) => {
     logger.debug(`Inside Get Task`);
     try {
-        const {id:taskID} = req.params
-        const task = await Tasks.findOne({ _id: taskID});
-        logger.debug(` Retrived task is ${task}`)
+        const { id: taskID } = req.params;
+        const task = await Tasks.findOne({ _id: taskID });
+        logger.debug(` Retrived task is ${task}`);
         if (!task) {
-            return res
-                .status(404)
-                .json({
-                    msg: `Task doesn't exit with that id:${taskID}`,
-                });
+            return res.status(404).json({
+                msg: `Task doesn't exit with that id:${taskID}`,
+            });
         }
         return res.status(200).json({ task });
     } catch (error) {
-        return res.status(500).json({msg:error})
+        return res.status(500).json({ msg: error });
     }
     // return res.status(200).json({ msg: 'Single Task returned' });
 };
 
-exports.updateTask = async(req, res) => {
+exports.updateTask = async (req, res) => {
     logger.debug(`Inside update Task`);
     try {
-        const {id:taskID}= req.params
-        const task = await Tasks.findByIdAndUpdate({_id:taskID},req.body,{
-            new:true,
-            runValidators:true,
-        })
-        if(!task)
-        {
-            return res.status(404).json({msg:`Task doesn't exist with Id ${taskID}`})
+        const { id: taskID } = req.params;
+        const task = await Tasks.findByIdAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!task) {
+            return res
+                .status(404)
+                .json({ msg: `Task doesn't exist with Id ${taskID}` });
         }
-        res.status(200).json({taskID,data:req.body})
+        res.status(200).json({ taskID, task});
     } catch (error) {
-            return res.status(500).json({msg:error})        
+        return res.status(500).json({ msg: error });
     }
 };
 
-exports.deleteTask = async(req, res) => {
+exports.deleteTask = async (req, res) => {
     logger.debug(`Inside Delete Task`);
-    const {id:taskID}= req.params
+    const { id: taskID } = req.params;
     try {
-        const task = await Tasks.findOneAndDelete({_id:taskID})
-        if(!task)
-        {
-            return res.status(404).json({msg:`Task with doesn't exist with ID: ${taskID}`})
+        const task = await Tasks.findOneAndDelete({ _id: taskID });
+        if (!task) {
+            return res
+                .status(404)
+                .json({ msg: `Task with doesn't exist with ID: ${taskID}` });
         }
-        return res.status(200).json({task})
-        
+        return res.status(200).json({ task });
     } catch (error) {
-        return res.status(500).json({msg: error})
-        
+        return res.status(500).json({ msg: error });
     }
 };
